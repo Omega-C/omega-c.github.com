4419246227a49d521dd444652f58f5267fd0ddd2
Omega-C <42104762+Omega-C@users.noreply.github.com>
2021-02-05 07:51:11 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,20 @@
 //short little react hello world
 class Map extends React.Component {
-  defaults(property,def) {
-    return((property in this.props)?this.props[property]:def);
-  }
-  render() {
-    return(<iframe style={{width:this.defaults("width",500),height:this.defaults("height",500),border:0}} frameBorder={0} src={`https://www.google.com/maps/embed/v1/${this.defaults("type","search")}?q=${this.defaults("location",this.props.children)}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}/>);
-  }
+  defaults(property,def) {return((property in this.props)?this.props[property]:def);}
+  render() {return(<iframe style={{width:this.defaults("width",500),height:this.defaults("height",500),border:0}} frameBorder={0} src={`https://www.google.com/maps/embed/v1/${this.defaults("type","search")}?q=${this.defaults("location",this.props.children)}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}/>);}
 }
 
 class Application extends React.Component {
   constructor(props) {
     super(props);
-    this.state={place:"Red Lobster"};//its an inside joke
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.dealWithPos.bind(this),(p)=>{},{enableHighAccuracy:true});
-    }
-    else {
-      alert("results will not pertain :,(");
-      this.state.pos="";
-    }
+    this.state={place:"Red Lobster",pos:" nearby"};//its an inside joke
   }
-  dealWithPos(position) {
-    this.state.pos=` near ${position.coords.latitude}, ${position.coords.longitude}`
+  componentDidMount() {
+    if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(this.dealWithPos.bind(this),(p)=>{},{enableHighAccuracy:true});}
+    else {alert("results will not pertain :,(");}
   }
-  render() {
-    return (<Map {...this.props}>{`${this.state.place}${this.state.pos}`}</Map>);
-  }
+  dealWithPos(position) {this.setState((state)=>{return({...state,pos:` around ${position.coords.latitude}, ${position.coords.longitude}`})})}
+  render() {return (<Map {...this.props}>{`${this.state.place}${this.state.pos}`}</Map>);}
 }
 
-ReactDOM.render(<Application height={600}/>,document.getElementById("app"));
+ReactDOM.render(<Application height={600} type="place"/>,document.getElementById("app"));
